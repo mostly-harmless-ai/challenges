0a8a7abd545fe0a90aa27ac63d09cd252985a3ff
Alejandro Piad Morffis <apiad@apiad.net>
2021-06-30 19:26:42 -0400

    Update tasks script

--- scripts/make_tasks.py
+++ scripts/make_tasks.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import shutil
+import os
+
 
 project_folder = Path(__file__).parent.parent
 tasks_folder = project_folder / "tasks"
@@ -8,3 +10,12 @@ docs_folder = project_folder / "docs" / "tasks"
 for file in tasks_folder.rglob("Readme.md"):
     task_name = file.parent.name
     shutil.copy(file, docs_folder / (task_name + ".md"))
+
+for task in tasks_folder.iterdir():
+    deploy_dir = project_folder / "deploy" / task.name
+    shutil.rmtree(deploy_dir)
+    deploy_dir = shutil.copytree(task, deploy_dir)
+    os.chdir(deploy_dir)
+    os.system("git init && git add . && git commit -m 'Initial commit' && git branch -M main")
+    os.system(f"git remote add origin git@github.com:mostly-harmless-ai/{task.name}.git")
+    os.system(f"git push -f origin main")
